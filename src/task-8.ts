02845a159614075956596c5615e46e6f421ffb1c
Stanislav Hanchev <stanislav@smoothr.de>
2025-09-20 22:43:05 +0300

    fix task 8

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,18 +1,17 @@
 import axios from "axios";
 
 interface Post {
-  readonly id: number;
+  id: number;
   title: string;
   body: string;
 }
 
 async function fetchPosts(): Promise<Post[]> {
-  const response = await axios.get<Post[]>(
-    '<https://jsonplaceholder.typicode.com/posts>'
-  );
+  const { data } = await axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts");
   
-  return response.data;
+  return data;
 }
+
 
 fetchPosts().then((posts) => {
   console.log(posts[0].title);
